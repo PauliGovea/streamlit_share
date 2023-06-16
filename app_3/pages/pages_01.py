f54ca74c9cd547872550cab03e23b198bb7891a1
Paulina Govea <105111010+PauliGovea@users.noreply.github.com>
2023-06-15 23:04:58 -0600

    Add files via upload

---
+++ app_3/pages/pages_01.py
@@ -0,0 +1,15 @@
+import requests
+
+import streamlit as st
+
+import numpy as np
+
+
+st.set_page_config(page_title="Hello", page_icon="🤟🏼")
+
+
+st.markdown("# Streamlit is awesome! 👾")
+
+
+if st.button("click ;)"):
+    st. image ("raw_data/enterprise.png")
